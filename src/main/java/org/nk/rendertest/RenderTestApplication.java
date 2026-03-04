190666b826e3a2d702137f54215a0345d5d5e0d9
N1XGit <niilo.johannes.nk@gmail.com>
2026-03-04 11:15:58 +0200

    Json Request test

--- src/main/java/org/nk/rendertest/RenderTestApplication.java
+++ src/main/java/org/nk/rendertest/RenderTestApplication.java
@@ -2,7 +2,8 @@ package org.nk.rendertest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -15,12 +16,12 @@ public class RenderTestApplication {
         SpringApplication.run(RenderTestApplication.class, args);
     }
 
-    @GetMapping("/api/test")
-    public Map<String, String> testConnection() {
+    @PostMapping("/api/echo")
+    public Map<String, Object> echoJson(@RequestBody Map<String, Object> payload) {
         return Map.of(
-                "status", "UP",
-                "message", "Dockerized app works!",
-                "timestamp", String.valueOf(System.currentTimeMillis())
+                "status", "received",
+                "data" , payload,
+                "timestamp", System.currentTimeMillis()
         );
     }
 }
